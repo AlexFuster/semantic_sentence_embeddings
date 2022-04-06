390f88ec3b9ab5816b56d55cc7869b45938f29ce
Alex Fuster <alejandro.fuster@voicemod.net>
2022-04-06 20:47:11 +0200

    added bias computation to script

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from time import time
 import json
+from importlib_metadata import metadata
 from transformers import AutoTokenizer, AutoModel
 import torch
 import torch.nn as nn
@@ -17,12 +18,9 @@ BATCH_SIZE=128
 STOPWORDS=stopwords.words('english')
 #OUT_PATH='anisotropy_results.csv'
 OUT_PATH='anisotropy_results.csv'
-#MAX_LENGTH=32
-#POOLING='CLS'
-#MODEL_NAME="bert-base-uncased"
-#MODEL_NAME="princeton-nlp/unsup-simcse-bert-base-uncased"
-#MODEL_NAME="../unsup_prompt"
-#TXT_PATH='wiki1m_for_simcse.txt'
+#TASK='freq_bias'
+#TASK='case_bias'
+#TASK='subword_bias'
 
 class Timer:
     def __init__(self) -> None:
@@ -46,14 +44,23 @@ class Similarity(nn.Module):
     def forward(self, x, y):
         return self.cos(x, y) / self.temp
 
+def deterministic_shuffle(x):
+    np.random.seed(1234)
+    np.random.shuffle(x)
+
+def is_stopword(token):
+    return token in STOPWORDS
+
+def is_subword(token):
+    return token.startswith('#')
+
 class Evaluator():
     def __init__(self,config,results) -> None:
         self.config=config
         self.anisotropy_results=results
         self.similarity=Similarity()
-        with open('token_freqs.json','r') as f:
+        with open('token_freqs_2.json','r') as f:
             self.dataset_frequencies=json.load(f)
-        self.config['freq_bias']=False
 
     def get_model(self):
         model_name=self.config['model']
@@ -82,7 +89,7 @@ class Evaluator():
             stopwords_mask=np.ones(attention_mask.shape)
             for i,sent in enumerate(batch_tokens):
                 for j,token in enumerate(sent):
-                    if token in STOPWORDS:
+                    if is_stopword(token):
                         stopwords_mask[i,j]=0
             attention_mask=attention_mask*torch.from_numpy(stopwords_mask)
         return attention_mask
@@ -100,16 +107,25 @@ class Evaluator():
         embeddings=embeddings.detach().numpy()
         return embeddings
 
-    def compute_freqs(self,batch_tokens,attention_mask):
-        if self.config['freq_bias']:
-            freqs=[]
+    def compute_metadata(self,tokens_metadata,batch_tokens,attention_mask):
+        if 'bias' in TASK:
+            freq_misses=0
             for i in range(len(batch_tokens)):
                 for j in range(len(batch_tokens[i])):
                     if attention_mask[i,j]==1:
-                        freqs.append(self.dataset_frequencies[batch_tokens[i][j]])
-            return np.array(freqs)
+                        if TASK=='freq_bias':
+                            if batch_tokens[i][j] in self.dataset_frequencies.keys():
+                                tokens_metadata['freqs'].append(self.dataset_frequencies[ batch_tokens[i][j]])
+                                tokens_metadata['stopword_flag'].append(is_stopword(batch_tokens[i][j]))
+                            else: 
+                                freq_misses+=1
+                                attention_mask[i,j]=0
+                        elif TASK=='case_bias':
+                            tokens_metadata['lowcase_flag'].append(batch_tokens[i][j].islower())
+                        elif TASK=='subword_bias':
+                            tokens_metadata['subword_flag'].append(is_subword(batch_tokens[i][j]))
 
-    def process_batch(self,token_embeddings,token_freqs,batch):
+    def process_batch(self,token_embeddings,tokens_metadata,batch):
         model_out,attention_mask,batch_tokens=self.model(batch)
         if 'no_CLS' in self.config['pooling']:
             attention_mask[:,0]=0
@@ -117,51 +133,73 @@ class Evaluator():
             sep_mask=F.pad(attention_mask,[0,1])
             sep_mask=torch.minimum(sep_mask[:,1:]-sep_mask[:,:-1],torch.Tensor([0]))
             attention_mask=attention_mask+sep_mask
-
         attention_mask=self.remove_stopword_embeddings(batch_tokens,attention_mask)
+        self.compute_metadata(tokens_metadata,batch_tokens,attention_mask)
 
         model_out=list(map(lambda x: self.embedding2numpy(x,attention_mask),model_out))
         token_embeddings.append(model_out)
-        token_freqs.append(self.compute_freqs(batch_tokens,attention_mask))
 
     def make_embeddings(self,sentences):
         batch=[]
         token_embeddings=[]
-        token_freqs=[]
+        tokens_metadata={'freqs':[],'stopword_flag':[],'lowcase_flag':[],'subword_flag':[]}
         for sentence in tqdm(sentences):
             if not self.config['cased']:
                 sentence=sentence.lower()
             batch.append(' '.join(sentence.replace('\n', '').split()[:self.config['max_length']]))
             if len(batch) >= BATCH_SIZE:
-                self.process_batch(token_embeddings,token_freqs,batch)
+                self.process_batch(token_embeddings,tokens_metadata,batch)
                 batch = []
         if len(batch) >= 0:
-            self.process_batch(token_embeddings,token_freqs,batch)
+            self.process_batch(token_embeddings,tokens_metadata,batch)
         token_embeddings=list(zip(*token_embeddings))
-        return token_embeddings, token_freqs
+        return token_embeddings, tokens_metadata
 
-    def get_similarity_prompt(self,x,freqs):
+    def get_similarity_prompt(self,x,tokens_metadata):
         res_isot=0
+        layer,x=x
+        n=self.config['N']
         x=np.concatenate(x,axis=0)
-        np.random.seed(1234)
-        np.random.shuffle(x)
-        x=x[:self.config['N']]
+        deterministic_shuffle(x)
+        x=x[:n]
+        aux_metadata={}
+        for k in tokens_metadata.keys():
+            if len(tokens_metadata[k])>0:
+                aux_metadata[k]=np.array(tokens_metadata[k])
+                deterministic_shuffle(aux_metadata[k])
+                aux_metadata[k]=aux_metadata[k][:n]
+        tokens_metadata=aux_metadata
         x = torch.tensor(x)
-        n=x.shape[0]
-        results=np.zeros((n**2,2))
+        results_an=np.zeros((n,n))
 
-        for i in tqdm(range(n)):
+        for i in tqdm(range(n)):   
             aux_sim=self.similarity(x[i:i+1], x).detach().numpy()
             res_isot=res_isot+aux_sim.mean()
-            results[i*n:(i+1)*n,0]=aux_sim
+            results_an[i]=aux_sim
 
-        if self.config['freq_bias']:
+        if layer==12 and 'bias' in TASK:
+            np.save('an.npy',results_an)
+            for k in tokens_metadata.keys():
+                 np.save(f'{k}.npy',tokens_metadata[k])
+            sys.exit()
+
+        return res_isot/n
+
+    """
+    def compute_freq_diff(self,freqs):
+        n=self.config['N']
+        if TASK=='freq_bias':
+            results=np.zeros((n**2,))
             freqs=np.concatenate(freqs,axis=0)
+            #deterministic_shuffle(freqs)
+            freqs=freqs[:n]
             for i in tqdm(range(n)):
                 aux_freq_dif=abs(freqs[i:i+1]-freqs)
-                results[i*n:(i+1)*n,1]=aux_freq_dif
-            
-        return res_isot/n
+                results[i*n:(i+1)*n]=aux_freq_dif
+            return results
+        else:
+            return None
+    """
 
     def run_configuration(self):
         txt_name=self.config['dataset'].replace('.txt','')
@@ -186,17 +224,18 @@ class Evaluator():
 
         query=' & '.join(query)
 
-        if True:#self.anisotropy_results.shape[0]==0 or self.anisotropy_results.query(query).shape[0]==0:
+        if 'bias' in TASK or self.anisotropy_results.shape[0]==0 or self.anisotropy_results.query(query).shape[0]==0:
             timer=Timer()
             self.model=self.get_model()
             timer()
             sentences=self.read_wikisent()
             timer()
             print('Computing embeddings...')
-            token_embeddings, token_freqs=self.make_embeddings(sentences)
+            token_embeddings, tokens_metadata=self.make_embeddings(sentences)
             timer()
+            #freq_diff=self.compute_freq_diff(token_freqs)
             print('Computing similarities...')
-            per_layer_similarities=list(map(lambda x: self.get_similarity_prompt(x,token_freqs),tqdm(token_embeddings)))
+            per_layer_similarities=list(map(lambda x: self.get_similarity_prompt(x,tokens_metadata),tqdm(enumerate(token_embeddings))))
             timer()
             print(timer.get_times())
             print(per_layer_similarities)
